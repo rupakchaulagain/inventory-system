3a0f51ebe3c8f40b21de3bb0e64752b571959a7b
Rupak Chaulagain <rupakchaulagain@gmail.com>
2020-08-07 21:42:03 +0545

    Cor configuration added...

---
+++ src/main/java/com/inventory/configuration/CorsConfiguration.java
@@ -0,0 +1,60 @@
+package com.inventory.configuration;
+
+import com.google.common.collect.ImmutableList;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        final org.springframework.web.cors.CorsConfiguration corsConfig = new org.springframework.web.cors.CorsConfiguration();
+        corsConfig.setAllowCredentials(true);
+        corsConfig.addAllowedOrigin("*");
+//        corsConfig.addAllowedOrigin("http://uat-admin-appointment.cogenthealth.com.np");
+        corsConfig.addAllowedHeader("*");
+
+        corsConfig.addAllowedMethod("OPTIONS");
+        corsConfig.addAllowedMethod("HEAD");
+        corsConfig.addAllowedMethod("GET");
+        corsConfig.addAllowedMethod("PUT");
+        corsConfig.addAllowedMethod("POST");
+        corsConfig.addAllowedMethod("DELETE");
+        corsConfig.addAllowedMethod("PATCH");
+
+        corsConfig.setAllowedHeaders(ImmutableList.of(
+                "Access-Control-Allow-Origin",
+                "Authorization",
+                "Cache-Control",
+                "Content-Type",
+                "Set-Cookie",
+                "XSRF-TOKEN",
+                "x-xsrf-token",
+                "log-header",
+                "X-FORWARDED-FOR",
+                "User-Agent",
+                "Proxy-Client-IP",
+                "WL-Proxy-Client-IP",
+                "HTTP_CLIENT_IP",
+                "HTTP_X_FORWARDED_FOR"
+                ));
+
+        // This allow us to expose the headers
+        corsConfig.setExposedHeaders(
+                Arrays.asList(
+                        "Access-Control-Allow-Headers",
+                        "Access-Control-Allow-Origin",
+                        "Authorization, x-xsrf-token, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, " +
+                                "Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Set-Cookie, XSRF-TOKEN"));
+
+
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfig);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+}
